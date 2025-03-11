33c162b5bc0cf5ff644ffe69a77cb49de5812d45
SUDARK <2963502563@qq.com>
2025-03-11 21:43:57 +0800

    Sudark

--- src/main/java/sudark/rentland/BookController.java
+++ src/main/java/sudark/rentland/BookController.java
@@ -32,26 +32,30 @@ public class BookController implements Listener {
                 whole += s;
             }
 
-            String[] lands = whole.split("§l·土地详情\\|");
+            String[] lands = whole.split("§l-土地详情 \\|");
             for (String land : lands) {
+                //System.out.println("\n" + land);
                 String name = findLandName(land);
                 String time = findLandDuration(land);
                 String loser = findLandPlayer(land);
                 String LandID = findLandID(land);
+               // System.out.println(name + " " + time + " " + loser + " " + LandID);
+
+                if (name == null || time == null || LandID == null) continue;
 
                 List<List<String>> data = FileManager.readCSV(FileManager.landFile);
                 for (List<String> row : data) {
                     if ((row.get(2) + row.get(4)).equals(LandID)) {
-                        if (name == row.get(1) && time == row.get(0) && loser == null) return;
+                        if (name.equals(row.get(1)) && time.equals(row.get(0)) && loser == null) return;
 
-                        if (name != null && name != row.get(1)) {
+                        if (name != null && !name.equals(row.get(1))) {
                             pl.sendMessage("§7领地名已修改为§e§l" + name);
                             row.set(1, name);
                             FileManager.writeCSV(FileManager.landFile, data);
                             return;
                         }
 
-                        if (time != null && time != row.get(0)) {
+                        if (time != null && !time.equals(row.get(0))) {
                             int x = Integer.parseInt(row.get(2));
                             int x1 = Integer.parseInt(row.get(3));
                             int z = Integer.parseInt(row.get(4));
@@ -61,14 +65,15 @@ public class BookController implements Listener {
                             int dx = x1 - x, dz = z1 - z;
                             int area = dx * dz;
 
-                            int deltaT = (t - Integer.parseInt(time)) * area / 700;
+                            int deltaT = (t - Integer.parseInt(time)) * area / 700 - 1;
                             int lvl = pl.getLevel();
-                            if (deltaT > lvl) {
-                                pl.sendMessage("[§e领地§f] 等级不足，无法修改租赁时间");
+                            if (-deltaT > lvl) {
+                                pl.sendMessage("[§e领地§f] 等级不足，无法修改租赁时间\n" +
+                                        "目前每周租金为 "+area/100+" 级，以天数调整可能会有误差");
                                 return;
                             }
                             pl.giveExpLevels(deltaT);
-                            pl.sendMessage("[§e领地§f] 修改租赁时间为§e" + time + "§f天 | §b" + deltaT + "§f级");
+                            pl.sendMessage("[§e领地§f] 修改租赁时间为§e" + time + "§f天 | §b" + -deltaT + "§f级");
                             row.set(0, time);
                             FileManager.writeCSV(FileManager.landFile, data);
                             return;
@@ -101,7 +106,7 @@ public class BookController implements Listener {
     }
 
     public String findLandDuration(String str) {
-        Pattern pattern1 = Pattern.compile("-§0剩余租赁天数: §0§l(\\d+)");
+        Pattern pattern1 = Pattern.compile("-§0剩余租赁天数: §l(\\d+)");
         Matcher matcher1 = pattern1.matcher(str);
         if (matcher1.find()) {
             return matcher1.group(1);
@@ -119,7 +124,7 @@ public class BookController implements Listener {
     }
 
     public String findLandID(String str) {
-        Pattern pattern = Pattern.compile("\\[§6([+-]?\\d+)§0,§6§6([+-]?\\d+)§0]");
+        Pattern pattern = Pattern.compile("\\[§6([+-]?\\d+)§0,§6([+-]?\\d+)§0]");
         Matcher matcher = pattern.matcher(str);
         if (matcher.find()) {
             String x = matcher.group(1);
@@ -163,11 +168,11 @@ public class BookController implements Listener {
                 int dx = x1 - x, dz = z1 - z;
                 int area = dx * dz;
 
-                String P = "§l·土地详情|<" + name + ">" +
-                        "\n\n §6§lA§0:[§6" + x + "§0,§6§6" + z + "§0]\n " +
-                        "§0§lB§0:[§6" + x1 + "§0,§6§6" + z1 + "§0]\n\n" +
-                        " -§0面积: §0§l" + area + "\n " +
-                        " -§0剩余租赁天数: §0§l" + t +
+                String P = "§l-土地详情 | <" + name + ">" +
+                        "\n\n §6§lA§0:[§6" + x + "§0,§6" + z + "§0]" +
+                        "\n §0§lB§0:[§6" + x1 + "§0,§6" + z1 + "§0]§l\n\n" +
+                        "  -§0面积: §0§l" + area + "\n " +
+                        " -§0剩余租赁天数: §l" + t +
                         "\n\n  =§0雇主: §l" + owner +
                         "\n\n§7   \"服务器土地管理部\"";
                 bookMeta.addPage(P);
